28fdaf57fb98a31fd4e8c25309781f46f2ddeef3
lanyi233 <lanyi233@qq.com>
2025-08-15 19:11:50 +0800

    update apt search show version

--- modules/apt_module.py
+++ modules/apt_module.py
@@ -505,7 +505,7 @@ class PluginManagerModule(BaseModule):
         for source_id, data in grouped.items():
             message += f"<b>源: {data['source_name']} ({source_id})</b>\n"
             for module in data['modules']:
-                message += f"<blockquote><code>{source_id}/{module['id']}</code> - {module['name']}</blockquote>\n"
+                message += f"<blockquote><code>{source_id}/{module['id']}</code> - {module['name']}({module['version']})</blockquote>\n"
             message += "\n"
         
         await event.edit(message, parse_mode='html')
